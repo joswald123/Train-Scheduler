6c005a563ed54ad4106cbc290f3d567fc6f4a4fe
joswald123 <48632836+joswald123@users.noreply.github.com>
2019-06-17 22:28:51 -0700

    adding new rows & icons, added delete rows function

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,160 +1,120 @@
 
-  var config = {
-    apiKey: "AIzaSyDzyUkYGSaUmyj5FOKC0NQcvGz-1d1jWxg",
-    authDomain: "train-homework-c2836.firebaseapp.com",
-    databaseURL: "https://train-homework-c2836.firebaseio.com",
-    projectId: "train-homework-c2836",
-    storageBucket: "train-homework-c2836.appspot.com",
-    messagingSenderId: "345214794083",
-    appId: "1:345214794083:web:801482ba8c6e0801"
-  };
- 
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyDzyUkYGSaUmyj5FOKC0NQcvGz-1d1jWxg",
+  authDomain: "train-homework-c2836.firebaseapp.com",
+  databaseURL: "https://train-homework-c2836.firebaseio.com",
+  projectId: "train-homework-c2836",
+  storageBucket: "train-homework-c2836.appspot.com",
+  messagingSenderId: "345214794083",
+  appId: "1:345214794083:web:801482ba8c6e0801"
+};
 
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-    var trainName = "";
-    var destinationPlace = "";
-    var tFrecuency = 0;
-    var nextTrain = "";
-    var tMinutesTillTrain = "";
+var trainName = "";
+var destinationPlace = "";
+var tFrecuency = "";
+var nextTrain = "";
+var tMinutesTillTrain = "";
 
+// submited info by a button 
 
-
-$('#submitButton').on('click', function(event){
+$('#submitButton').on('click', function (event) {
   event.preventDefault();
-  
+
   var trainName = $("#trainName").val().trim();
   var destinationPlace = $("#destination").val().trim();
   var firstTrainTime = $("#firstTrain").val().trim();
   var tFrecuency = $("#frecuency").val().trim();
 
+
+
   var traintoAdd = {
-    trainName, 
+    trainName,
     destinationPlace,
     firstTrainTime,
-    tFrecuency,
-    nextTrain,
-    tMinutesTillTrain,
-  
+    tFrecuency
   }
-  
-
-  // Moment.js functions: 
-
-  // First Time (pushed back 1 year to make sure it comes before current tFrecuency)
-  var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
- 
-  // Difference between the times
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  
-  // Time apart (remainder)
-  var tRemainder = diffTime % tFrecuency;
-  
-  // Minute Until Train
-  var tMinutesTillTrain = tFrecuency - tRemainder;
-  
-  // Next Train
-  var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
- 
 
 
-  var newRow = $('<tr>').append(
-
-    $('<td>').text(trainName),
-    $('<td>').text(destinationPlace),
-    $('<td>').text(tFrecuency),
-    $('<td>').text(nextTrain),
-    $('<td>').text(tMinutesTillTrain),
-    $('<td>').append('<i class="fas fa-eraser"></i>'),
-    $('<td>').append('<i class="fas fa-edit"></i>'),
-
-  
- 
-  )
-
-    $('.table').append(newRow)
-    
-    
-   
-    database.ref().set({
-     trainName: trainName,
-     destinationPlace: destinationPlace,
-     tFrecuency: tFrecuency,
-     nextTrain: nextTrain,
-     tMinutesTillTrain: tMinutesTillTrain,
-    });
+  database.ref().push(traintoAdd);
 
 
 })
 
-database.ref().on("value", function(snapshot) {
+database.ref().on("value", function (snapshot) {
+   console.log(snapshot.val())
+  const data = snapshot.val();
+  $("#trainResults").empty();
+  // creating a new Row with input info
+  for (let key in data) {
+    console.log("I'm in the data loop")
+    var $newRow = $('<tr>')
+    $newRow.append($('<td>').text(data[key].trainName))
+    $newRow.append($('<td>').text(data[key].destinationPlace))
+    $newRow.append($('<td>').text(data[key].tFrecuency))
+    console.log($newRow)
+    // Moment.js functions: 
 
-  console.log(snapshot.val());
-  console.log(snapshot.val().trainName);
-  console.log(snapshot.val().destinationPlace);
-  console.log(snapshot.val().tFrecuency);
-  console.log(snapshot.val().nextTrain);
-  console.log(snapshot.val().tMinutesTillTrain);
+    // First Time (pushed back 1 year to make sure it comes before current tFrecuency)
+    var firstTimeConverted = moment(data[key].firstTrainTime, "HH:mm").subtract(1, "years");
+    
 
- 
-  $(".Train_Name").text(snapshot.val().trainName);
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % data[key].tFrecuency;
+
+    // Minute Until Train
+    var tMinutesTillTrain = data[key].tFrecuency - tRemainder;
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+
+    $newRow.append($('<td>').text(nextTrain))
+    $newRow.append($('<td>').text(tMinutesTillTrain))
+    $newRow.append($('<td>').append('<i class="fas fa-eraser"></i>'))
+    $newRow.append($('<td>').append('<i class="fas fa-edit"></i>'))
+
+    $("#trainResults").append($newRow);
+  }
+
+// update time
+
+function updateTime () {
   
-  // $(".Train_Name").text(snapshot.val().destinationPlace);
-  // $(".Train_Name").text(snapshot.val().tFrecuency);
-  // $(".Train_Name").text(snapshot.val().nextTrain);
-  // $(".Train_Name").text(snapshot.val().tMinutesTillTrain);
+  const now = moment();
+  const minutes = now.format("mm");
 
-}, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    });
+  console.log(minutes);
+
+  
+}
+
+setInterval (updateTime, 1000);
+updateTime();
 
 
-    function funcMain()
-    {
-      $("#add_row").on('click',newRowTable);
-    
-      $("loans_table").on('click','.fa-eraser',deleteProduct);
-      $("loans_table").on('click','.fa-edit',editProduct);
-    
-      $("body").on('click',".fa-eraser",deleteProduct);
-      $("body").on('click',".fa-edit",editProduct);
-    }
-    
-    
-    function functionDeleteInfo(){
-      //Obteniendo la fila que se esta eliminando
-      var a=this.parentNode.parentNode;
-      //Obteniendo el array de todos loe elementos columna en esa fila
-      //var b=a.getElementsByTagName("td");
-      var cantidad=a.getElementsByTagName("td")
-      console.log(a);
-    
-      $(this).parent().parent().fadeOut("slow",function(){$(this).remove();});
-    }
-    
-    
-    function deleteProduct(){
-      //Guardando la referencia del objeto presionado
-      var _this = this;
-      //Obtener las filas los datos de la fila que se va a elimnar
-      var array_fila=getRowSelected(_this);
-    
-      //Restar esos datos a los totales mostrados al finales
-      //calculateTotals(cantidad, precio, subtotal, impuesto, totalneto, accioneliminar)
-      calculateTotals(array_fila[3],array_fila[4],array_fila[5],array_fila[6],array_fila[7],2)
-    
-      $(this).parent().parent().fadeOut("slow",function(){$(this).remove();});
-    }
-    
-    
-    function editProduct(){
-      var _this = this;;
-      var array_fila=getRowSelected(_this);
-      console.log(array_fila[0]+" - "+array_fila[1]+" - "+array_fila[2]+" - "+array_fila[3]+" - "+array_fila[4]+" - "+array_fila[5]+" - "+array_fila[6]+" - "+array_fila[7]);
-      //Codigo de editar una fila lo pueden agregar aqui
-    } 
-
-    
 // create an event listening for an icon. inside on.click function = delete function or edit funtion 
+
+$(document).on('click', '.fa-eraser', function (event) {
+    event.preventDefault();
+    $(this).closest('tr').remove();
+  
+});
+
+// $(document).on('click', '.fa-edit', function (event) {
+//   event.preventDefault();
+
+//   $('td',$newRow).each(function() { $(this).html('<input type="text" value="' + $(this).html() + '" />'); 
+
+  
+//   });
+
+// });
+
+
+});
